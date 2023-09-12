62eb77e2f74819923e6c17c393f87b0e229aba68
Haznto <haznto@gmail.com>
2023-09-12 18:19:24 +0000

    lab33 done

--- src/Components/Todo/index.jsx
+++ src/Components/Todo/index.jsx
@@ -5,14 +5,20 @@ import { v4 as uuid } from 'uuid';
 import { Title, Grid, Flex, Button, TextInput, Text, Slider } from '@mantine/core';
 import List from '../List/List';
 import { ListContext } from '../Context/ListOfData/ListOfData';
+import LoginForm from '../LoginForm/LoginForm';
+import Auth from '../auth/Auth';
+import { LoginContext } from '../Context/LoginContext/LoginContext';
+
 
 
 const Todo = () => {
+
   // const listContext = React.createContext()
   const [defaultValues] = useState({
     difficulty: 4,
   });
   const { data, dispatch } = useContext(ListContext)
+  const { can } = useContext(LoginContext)
   // const [list, setList] = useState([]);
   const [incomplete, setIncomplete] = useState([]);
   const { handleChange, handleSubmit } = useForm(addItem, defaultValues);
@@ -32,16 +38,18 @@ const Todo = () => {
   }
 
   function toggleComplete(id) {
+    if (can('update')) {
 
-    const items = data.list.map(item => {
-      if (item.id === id) {
-        item.complete = !item.complete;
-      }
-      return item;
-    });
+      const items = data.list.map(item => {
+        if (item.id === id) {
+          item.complete = !item.complete;
+        }
+        return item;
+      });
 
-    // setList(items);
-    dispatch({ type: 'replaceList', payload: items })
+      // setList(items);
+      dispatch({ type: 'replaceList', payload: items })
+    }
 
   }
 
@@ -57,46 +65,52 @@ const Todo = () => {
 
   return (
     <Flex direction='column' justify='center' align={'center'} mih='80vh'>
+      <LoginForm />
       {/* <header data-testid="todo-header"> */}
-      <Title ta={'center'} c={'white'} bg={'#343a40'} w='80%' p={"20px"} m={'auto'} data-testid="todo-h1" order={1}>To Do List: {incomplete} items pending</Title>
-      {/* </header > */}
+      <Auth capability="read">
+        <Title ta={'center'} c={'white'} bg={'#343a40'} w='80%' p={"20px"} m={'auto'} data-testid="todo-h1" order={1}>To Do List: {incomplete} items pending</Title>
+        {/* </header > */}
+      </Auth>
       <Grid mih={'80vh'} justify='center' w={'80%'} grow gutter={5} gutterXs="md" gutterMd="xl" gutterXl={50} >
         <Grid.Col span={4}>
-          <form onSubmit={handleSubmit}>
+          <Auth capability="create">
+            <form onSubmit={handleSubmit}>
 
-            <h2>Add To Do Item</h2>
+              <h2>Add To Do Item</h2>
 
-            <TextInput
-              onChange={handleChange}
-              name="text"
-              placeholder="Task Details"
-              label="To Do Item"
-            />
+              <TextInput
+                onChange={handleChange}
+                name="text"
+                placeholder="Task Details"
+                label="To Do Item"
+              />
 
-            <TextInput
-              onChange={handleChange}
-              name="assignee"
-              placeholder="For who?"
-              label="Assigned To"
-            />
+              <TextInput
+                onChange={handleChange}
+                name="assignee"
+                placeholder="For who?"
+                label="Assigned To"
+              />
 
-            <Text>Difficulty</Text>
-            <Slider
-              color='indigo'
-              onChange={handleChange}
-              defaultValue={defaultValues.difficulty}
-              step={1}
-              min={1}
-              max={5}
-              name="difficulty"
-            />
-            <Button color='indigo' type="submit">Add Task</Button>
-          </form>
+              <Text>Difficulty</Text>
+              <Slider
+                color='indigo'
+                onChange={handleChange}
+                defaultValue={defaultValues.difficulty}
+                step={1}
+                min={1}
+                max={5}
+                name="difficulty"
+              />
+              <Button color='indigo' type="submit">Add Task</Button>
+            </form>
+          </Auth>
         </Grid.Col>
         <Grid.Col span={8}>
           {/* <Pagination m={'20px'} color="indigo" total={10} /> */}
-
-          <List list={data.list} toggleComplete={toggleComplete} deleteItem={deleteItem} />
+          <Auth capability="read">
+            <List list={data.list} toggleComplete={toggleComplete} deleteItem={deleteItem} />
+          </Auth>
           {/* {
             list.map(item => (
               <div key={item.id}>
